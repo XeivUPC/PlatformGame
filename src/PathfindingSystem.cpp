22ee77889976ccf1c6415df5d7e505a3ad261010
Ana Alcázar Cobo <ana.alcazar@estudiantat.upc.edu>
2024-11-22 13:20:07 +0100

    Update

--- src/PathfindingSystem.cpp
+++ src/PathfindingSystem.cpp
@@ -22,14 +22,121 @@ PathfindingSystem::PathfindingSystem()
 	currentPathfinder = 0;
 }
 
+PathfindingSystem::~PathfindingSystem()
+{
+}
+
+Pathfinder::Pathfinder()
+{
+	target = getPositionTile();
+}
+
+Pathfinder::~Pathfinder()
+{
+}
+
 Vector2D Pathfinder::toTile(Vector2D position)
 {
 	return { position.getX() / 16, position.getY() / 16 };
 }
 
+Vector2D Pathfinder::getTargetTile()
+{
+	return target;
+}
+
+Vector2D Pathfinder::getPositionTile()
+{
+	return toTile(entityAttached->position);
+}
+
+void Pathfinder::SetTarget(Entity* entity)
+{
+	entityTarget = entity;
+	RefreshTargetPosition();
+}
+
+void Pathfinder::SetTarget(Vector2D tile)
+{
+	target = tile;
+	entityTarget = nullptr;
+}
+
+void Pathfinder::Connect(Entity* entity)
+{
+	entityAttached = entity;
+	SetTarget(entity);
+	path.clear();
+}
+
+void Pathfinder::Disconnect()
+{
+	entityAttached = nullptr;
+	entityTarget = nullptr;
+}
+
+bool Pathfinder::HasArrivedTarget()
+{
+	if (entityAttached->position.getX() == target.getX() && entityAttached->position.getY() == target.getY())
+		return true;
+	return false;
+}
+
+void Pathfinder::ShortenPath()
+{
+	int size = 0;
+	for (int i = 0; i < path.size(); i++)
+	{
+		if (path.at(i).getX() == getPositionTile().getX() && path.at(i).getY() == getPositionTile().getY())
+		{
+			size = i;
+			break;
+		}
+	}
+	for (int i = 0; i < path.size() - size; i++)
+	{
+		path.at(i) = path.at(i + size);
+	}
+	size = path.size() - size;
+	for (int i = size-1; i >= 0; i--)
+	{
+		if (path.at(i).getX() == getTargetTile().getX() && path.at(i).getY() == getTargetTile().getY())
+		{
+			size -= i;
+			path.resize(size);
+			break;
+		}
+	}
+}
+
+bool Pathfinder::IsTileInPath(Vector2D tile)
+{
+	for (size_t i = 0; i < path.size(); i++)
+	{
+		if (path.at(i).getX() == tile.getX() && path.at(i).getY() == tile.getY())
+			return true;
+	}
+	return false;
+}
+
+void Pathfinder::RefreshTargetPosition()
+{
+	if (entityTarget == nullptr)return;
+	target = toTile(entityTarget->position);
+}
+
 bool Pathfinder::NeedsToUpdate()
 {
 	return needsToUpdate;
+}
+
+void Pathfinder::Update(float dt)
+{
+	RefreshTargetPosition();
+	if (IsTileInPath(target) && IsTileInPath(entityAttached->position))
+		ShortenPath();
+	else
+		needsToUpdate = true;
 }
 
 bool PathfindingSystem::Update(float dt)
--- src/PathfindingSystem.h
+++ src/PathfindingSystem.h
@@ -1,28 +1,51 @@
 #include "Module.h"
 #include "LevelSection.h"
 
+enum AXIS_RESTRICTION
+{
+	AXIS_FREE,
+	AXIS_BORDER_RESTRAINT,
+	AXIS_BLOCK,
+};
+
 class Pathfinder
 {
 	friend class PathfindingSystem;
 private:
-	bool needsToUpdate;
+	AXIS_RESTRICTION axis_restriction[2] = {AXIS_FREE, AXIS_FREE};
+	Entity* entityAttached = nullptr;
+	Entity* entityTarget = nullptr;
+	bool needsToUpdate = false;
 	std::vector<Vector2D> path;
 	Vector2D target;
+	bool pause;
+	bool isMoving;
+	void ShortenPath();												//Done
+	bool IsTileInPath(Vector2D tile);								//Done
+	void RefreshTargetPosition();									//Done
+	void SetNearestTileOfTarget();
 protected:
-	bool NeedsToUpdate();
-	void Update(float dt);
+	bool NeedsToUpdate();											//Done
+	void Update(float dt);											//Done
 public:
-	Pathfinder();
+	Pathfinder();													//Done
 	~Pathfinder();
 	Vector2D toTile(Vector2D position);
 	Vector2D getTargetTile();
 	Vector2D getPositionTile();
-	void Connect();
+	void SetTarget(Entity* entity);
+	void SetTarget(Vector2D tile);
+	void Connect(Entity* entity);
 	void Disconnect();
+
+	bool HasArrivedTarget();
+	bool IsMoving();
+	bool IsPaused();
 };
 
 class PathfindingSystem : public Module
 {
+	friend class Pathfinder;
 private:
 	int currentPathfinder;
 	std::vector<Pathfinder*> pathfinderObjects;
@@ -30,15 +53,14 @@ private:
 	std::vector<Vector2D> alreadyCheckedTiles;
 	int GetSectionTileValue(Vector2D tile);
 	int CalculateTilePriority(Vector2D tile);
+protected:
+	Vector2D GetTileOfEntity(Entity* entity);
+	std::vector<Vector2D> GetRestrictedGroupTiles();
 public:
 	PathfindingSystem();
 	~PathfindingSystem();
 
-	bool Awake()override;
 	bool Start()override;
-	bool PreUpdate()override;
 	bool Update(float dt)override;
-	bool PostUpdate()override;
 	bool CleanUp()override;
-
 };
